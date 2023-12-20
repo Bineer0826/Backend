405017712726f7d5d689d9c6c7192328e7ddc3d0
Gabin Park <loveqls1004@gmail.com>
2023-12-20 18:48:19 +0900

    3

--- JavaSyntax/AbstractClassInterface/CreateObjectOfInterface_1.java
+++ JavaSyntax/AbstractClassInterface/CreateObjectOfInterface_1.java
@@ -1,4 +1,22 @@
 package JavaSyntax.AbstractClassInterface;
+//방법 1 : 자식클래스를 만들어서 객체생성
+interface H{
+    int a = 3;
+    void abc();
+}
+class I implements H{
+    @Override
+    public void abc() {
+        System.out.println("자식클래스로 객체 생성");
+    }
+}
 
 public class CreateObjectOfInterface_1 {
+    public static void main(String[] args) {
+        //객체생성
+        I i = new I();
+
+        //메서드호출
+        i.abc();
+    }
 }
---
+++ JavaSyntax/AbstractClassInterface/DefaultMethod_2.java
@@ -0,0 +1,22 @@
+package JavaSyntax.AbstractClassInterface;
+interface MM {
+    default void abc(){
+        System.out.println("MM인터페이스의 abc()");
+    }
+}
+class TT implements MM {
+    //완성된 메서드만 있어서 인터페이스를 클래스가 상속해도 문제가 없다
+
+    @Override
+    public void abc() {
+        System.out.println("TT클래스의 abc()");
+}
+public class DefaultMethod_2 {
+    public static void main(String[] args) {
+        //객체 생성
+        TT t1 = new TT();
+        //메서드 호출
+        t1.abc();//
+    }
+}
+}
