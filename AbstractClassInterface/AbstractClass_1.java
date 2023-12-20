a0dc8a98dc09cdb79827434c53f9f020dfc18ffb
Gabin Park <loveqls1004@gmail.com>
2023-12-20 15:55:07 +0900

    인터페이스

--- AbstractClassInterface/AbstractClass_1.java
+++ AbstractClassInterface/AbstractClass_1.java
@@ -1,6 +1,6 @@
 package AbstractClassInterface;
 //추상클래스의 객체생성 방법
-//자식클래스 생성해서 객체생성
+//방법 1 : 자식클래스 생성해서 객체생성
 abstract class A {
     abstract void abc();
 }
@@ -10,13 +10,11 @@ class B extends A {
     }
 }
 
-public class AbstractClass {
+public class AbstractClass_1 {
     public static void main(String[] args) {
         //1.객체생성
-        B b1 = new B();//추상메서드를 포함한 클래스로 객체생성
-        A b2 = new B();//A타입으로 해도 B가 A를 상속받아서 상관없이 가능하다
-        //2.메서드호출
+        B b1 = new B();//b는 a여서 a를 포함하므로 a객체를 만든 것과 같다
+        //2. 메서드 호출
         b1.abc();
-        b2.abc();
     }
 }
---
+++ AbstractClassInterface/InheritanceOfInterface_1.java
@@ -0,0 +1,19 @@
+package AbstractClassInterface;
+
+interface F {}
+interface G {}
+
+//단일 인터페이스 상속
+class AA implements F {
+
+}
+//다중 인터페이스 상속
+class BB implements F,G {
+
+}
+//클래스와 인터페이스 같이 상속
+class CC extends AA implements G{
+    //F가 상속되고 g가 상속되었다.
+}
+public interface InheritanceOfInterface_1 {
+}
